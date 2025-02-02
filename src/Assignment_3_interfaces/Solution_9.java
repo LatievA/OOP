34d13039daf3dbea2b36602379a213ccba5dd94c
LatiyevA <abilailat@gmail.com>
2025-02-02 21:56:14 +0500

    assignment3 v2

--- src/Assignment_3_interfaces/Solution_9.java
+++ src/Assignment_3_interfaces/Solution_9.java
@@ -2,7 +2,7 @@ package Assignment_3_interfaces;
 
 import java.io.IOException;
 
-public class Solution_9 {
+public class  Solution_9 {
     public static void main(String[] args) throws Exception {
     }
 
